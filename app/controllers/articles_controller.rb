ecc02667b8adadbe62256bb20d1f5966ab9380a7
JuanSAuzaO <sebastianauza@hotmail.com>
2022-12-21 11:39:24 -0500

    Finished new, create, edit and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,42 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
