5d652a4048e63cd7f13409a6908b2f2e7eb764e7
JuanSAuzaO <sebastianauza@hotmail.com>
2022-12-26 21:45:33 -0500

    Worked on styling my views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,23 @@ import "bootstrap"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+require("@popperjs/core")
+
+// Import the specific modules you may need (Modal, Alert, etc)
+import { Tooltip, Popover } from "bootstrap"
+
+// If you're using Turbolinks. Otherwise simply use: jQuery(function () {
+document.addEventListener("turbolinks:load", () => {
+    // Both of these are from the Bootstrap 5 docs
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    })
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new Popover(popoverTriggerEl)
+    })
+})
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
+// Add the following lines
+const webpack = require("webpack")
 
-environment.plugins.append('Provide',new webpack.ProvidePlugin({
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
+    Popper: ['popper.js', 'default']  // Not a typo, we're still using popper.js here
+}))
+// End new addition
 
 module.exports = environment
