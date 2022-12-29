f2e3497056772c505e0e7ac086e9bba1220bcf1c
JuanSAuzaO <sebastianauza@hotmail.com>
2022-12-28 20:14:29 -0500

    Add edit and update methods to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
   
   def create
     @user = User.new(user_params)
@@ -11,6 +15,16 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:notice] = "Your account information was updated successfully"
+        redirect_to articles_path
+      else
+        render :edit
+      end
   end
 
   private
