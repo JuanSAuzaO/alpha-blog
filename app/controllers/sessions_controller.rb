09e5782701be7439e2f69d25681c1fe24c66764a
JuanSAuzaO <sebastianauza@hotmail.com>
2022-12-29 21:42:19 -0500

    Added create and destroy sessions functionalities

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in succesfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "Invalid credentials"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
+  end
+end
