5f7b3e1ecd7c05a80b5455ecc538b3869279ce20
JuanSAuzaO <sebastianauza@hotmail.com>
2023-01-01 11:10:59 -0500

    Added restrictions to actions in both articles and users controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,11 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!current_user
   end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to perform that action."
+      redirect_to login_path
+    end
+  end
 end
