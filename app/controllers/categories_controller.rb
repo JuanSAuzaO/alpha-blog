229653899635c1827e06fd400a126da6fd9deea4
JuanSAuzaO <sebastianauza@hotmail.com>
2023-01-02 11:41:51 -0500

    Finished categories controller tests

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,28 @@
+class CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was succesfully created"
+      redirect_to @category
+    else
+      render :new
+    end
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
